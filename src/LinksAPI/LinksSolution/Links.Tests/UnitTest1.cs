0200436f64587ff1d724c6915a526303426507a2
Charlie Tran <Charlietranpro@gmail.com>
2025-09-08 20:44:23 +0000

    End of Day1

---
+++ src/LinksAPI/LinksSolution/Links.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace Links.Tests;
+
+public class UnitTest1
+{
+    [Fact] // Attribute Like ([HttpPost("/links")]
+    public void Test1()
+    {
+        int a = 10, b = 20, answer;
+        answer = a + b;
+        Assert.Equal(30, answer);
+    }
+}
