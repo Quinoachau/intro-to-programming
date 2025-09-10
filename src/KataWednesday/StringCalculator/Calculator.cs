ba6a098572428d592c179d13a2ad4917f03298de
Charlie Tran <Charlietranpro@gmail.com>
2025-09-10 16:44:43 -0400

    End of Day 3

---
+++ src/KataWednesday/StringCalculator/Calculator.cs
@@ -0,0 +1,29 @@
+﻿
+using Xunit.Sdk;
+
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        if (numbers == "" || numbers.Length == 0)
+        {
+            return 0;
+        }
+        else if (numbers.Length == 1) 
+        {
+            return int.Parse(numbers);
+        }
+        else
+        {
+            string[] numbersArray = numbers.Split(',','\n');
+            var parsedNumbers = numbersArray.Select(int.Parse);
+            if (parsedNumbers.Any(num => num < 0))
+            {
+                throw new ArgumentException("Negative numbers are not allowed.");
+            }
+            int sum = parsedNumbers.Sum();
+
+            return sum;
+        }
+    }
+}
---
+++ src/KataWednesday/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace StringCalculator;
+public class CalculatorTests
+{
+    [Theory]
+    [InlineData("1\n2", 3)]
+    [InlineData("1\n2, 3", 6)]
+    [InlineData("8\n8, 10", 26)]
+    public void EmptyStringReturnsZero(string numbers, int actualNumber)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(actualNumber, result);
+    }
+}
