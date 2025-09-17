70ef33ede69cfdd04fe730ecc8910b35166fd667
Charlie Tran <Charlietranpro@gmail.com>
2025-09-17 13:51:32 -0400

    post lunch

---
+++ instructor/src/Links/frontend/src/links/links.ts
@@ -0,0 +1,15 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { List } from './pages/list';
+
+@Component({
+  selector: 'app-links',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [List],
+  template: `
+    <p>Links Will Go Here</p>
+
+    <app-links-list />
+  `,
+  styles: ``,
+})
+export class Links {}
---
+++ src/Links/frontend/src/links/types.ts
@@ -0,0 +1,8 @@
+export type LinkListItemModel = {
+  id: string;
+  title: string;
+  href: string;
+  description: string;
+  addedBy: string;
+  created: string;
+};
